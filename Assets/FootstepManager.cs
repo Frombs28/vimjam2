422d56370880a0384b8a075d57bbc3b0b0088edf
castrj5 <castrj25@gmail.com>
2021-09-14 19:17:47 -0400

    footsteps

--- Assets/FootstepManager.cs
+++ Assets/FootstepManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class FootstepManager : MonoBehaviour
 {
+    [FMODUnity.EventRef]
+    public string inputSound;
+    public float walkSpeed = 1f;
+
+    private bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("PlayFootsteps", 0, walkSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+    private void PlayFootsteps()
+    {
+        if (isMoving)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(inputSound, transform.position);
+        }
+    }
+
+    private void OnDisable()
+    {
+        isMoving = false;
     }
 }
