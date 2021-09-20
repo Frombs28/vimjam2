368b23fc72b57be2d1724f6ca9efe58aa93e77ea
castrj5 <castrj25@gmail.com>
2021-09-19 23:25:00 -0400

    finished main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    [FMODUnity.EventRef]
+    public string buttonSFX;
     public GameObject creditsSet;
     public GameObject mainSet;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,23 +24,27 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
+        FMODUnity.RuntimeManager.PlayOneShot(buttonSFX, transform.position);
         SceneManager.LoadScene(1);
     }
 
     public void OpenCredits()
     {
+        FMODUnity.RuntimeManager.PlayOneShot(buttonSFX, transform.position);
         creditsSet.SetActive(true);
         mainSet.SetActive(false);
     }
 
     public void OpenMain()
     {
+        FMODUnity.RuntimeManager.PlayOneShot(buttonSFX, transform.position);
         creditsSet.SetActive(false);
         mainSet.SetActive(true);
     }
 
     public void Exit()
     {
+        FMODUnity.RuntimeManager.PlayOneShot(buttonSFX, transform.position);
         Application.Quit();
     }
 }
