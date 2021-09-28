7065775cf26bdffb55757d0b49152d891aa54099
castrj5 <castrj25@gmail.com>
2021-09-28 12:27:54 -0400

    my unity bugged out

--- Assets/RadioManager.cs
+++ Assets/RadioManager.cs
@@ -1,9 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using System.Collections;
 
 public class RadioManager : MonoBehaviour
 {
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class ClipInfo
+    {
+        public string name = "";
+    }
 
     [FMODUnity.EventRef]
     public string staticBackground;
@@ -13,31 +20,54 @@ public class RadioManager : MonoBehaviour
     public string voiceLines;
 
     [HideInInspector]
+    public ClipInfo clipInfo;
+    GCHandle clipHandle;
+
+    [HideInInspector]
     public FMOD.Studio.EventInstance staticInstance;
     [HideInInspector]
     public FMOD.Studio.EventInstance noiseInstance;
     [HideInInspector]
     public FMOD.Studio.EventInstance voiceInstance;
+
+    FMOD.Studio.EVENT_CALLBACK playCallback;
+    string currentPlayingVoiceClip;
     // Start is called before the first frame update
     void Start()
     {
+        playCallback = new FMOD.Studio.EVENT_CALLBACK(PlayEventCallback);
+
+
         staticInstance = FMODUnity.RuntimeManager.CreateInstance(staticBackground);
         noiseInstance = FMODUnity.RuntimeManager.CreateInstance(radioNoises);
         voiceInstance = FMODUnity.RuntimeManager.CreateInstance(voiceLines);
         staticInstance.start();
         noiseInstance.start();
+
+        clipHandle = GCHandle.Alloc(clipInfo, GCHandleType.Pinned);
+        voiceInstance.setUserData(GCHandle.ToIntPtr(clipHandle));
+
+        voiceInstance.setCallback(playCallback, FMOD.Studio.EVENT_CALLBACK_TYPE.SOUND_PLAYED);
         voiceInstance.start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(clipInfo.name);
+    }
+
+    void OnDestroy()
+    {
+        voiceInstance.setUserData(IntPtr.Zero);
+        voiceInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        voiceInstance.release();
+        clipHandle.Free();
     }
 
     public void RadioActive(bool t)
     {
-        if(t)
+        if (t)
         {
             staticInstance.setVolume(1);
             noiseInstance.setVolume(1);
@@ -81,5 +111,28 @@ public class RadioManager : MonoBehaviour
         staticInstance.setPaused(b);
         noiseInstance.setPaused(b);
         voiceInstance.setPaused(b);
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT PlayEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
+    {
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+        // Retrieve the user data
+        IntPtr clipInfoPtr;
+        FMOD.RESULT result = instance.getUserData(out clipInfoPtr);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogError("Timeline Callback error: " + result);
+        }
+        else if (clipInfoPtr != IntPtr.Zero)
+        {
+            // Get the object to store beat and marker details
+            GCHandle clipHandle = GCHandle.FromIntPtr(clipInfoPtr);
+            ClipInfo clipInfo = (ClipInfo)clipHandle.Target;
+
+            var parameter = (FMOD.Studio.SOUND_INFO)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.SOUND_INFO));
+            clipInfo.name = parameter.name;
+        }
+        return FMOD.RESULT.OK;
     }
 }
